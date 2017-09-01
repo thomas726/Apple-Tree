9af4cedabfc645c82a6b4a068eae82293e3bb579
Thomas Mitchell <twm18@pitt.edu>
2017-08-31 23:14:02 -0400

    Apple Tree
    intial push to github

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+	// prefab for instanting apples
+	public GameObject applePrefab;
+
+	//speed at which the AppleTree move in Meters / second
+	public float speed = 1f;
+	//distance where apple tree turns around
+	public float leftAndRightEdge = 10f;
+	//chance that tree will chaneg direction
+	public float chanceToChangeDirection = 0.1f;
+	//rate at which apples will be insantiated
+	public float secondsBetweenAppleDrops = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+		//start dropping apples every seconds 	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//handle basic movement
+		//change direction
+		
+	}
+}
